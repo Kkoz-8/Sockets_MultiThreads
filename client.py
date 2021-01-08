3a1bc24031af069c59bc6f37baf2fbc3e2134136
Kkoz-8 <61071287+Kkoz-8@users.noreply.github.com>
2021-01-07 21:54:57 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+
+SERVER = "127.0.0.1"
+PORT = 14000
+HEADER = 100
+FORMAT = "utf-8"
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER, PORT))
+print("Version -- 1.1\n")
+  
+
+def Receive_msg():
+    while True:
+        username_header = client_socket.recv(HEADER)
+        username_length = int(username_header.decode(FORMAT))
+        username = client_socket.recv(username_length).decode(FORMAT)
+
+        msg_header = client_socket.recv(HEADER)
+        if msg_header:
+            msg_length = int(msg_header.decode(FORMAT))
+            message = client_socket.recv(msg_length).decode(FORMAT)
+            print(f"{username} > {message}")
+        else:
+            print("Nothing received")
+
+
+def Send_msg():
+    while True:
+        user = str(input("Enter Username: "))
+        if user:
+            username = user.encode(FORMAT)
+            username_length = str(len(user)).encode(FORMAT)
+            username_length += b" " * (HEADER - len(username_length))
+            client_socket.send(username_length + username)
+            break
+        else:
+            continue
+
+    while True:
+        _input = str(input())
+        if _input:
+            message = _input.encode(FORMAT)
+            message_length = str(len(_input)).encode(FORMAT)
+            message_length += b" " * (HEADER - len(message_length))
+            client_socket.send(message_length + message)
+
+
+send_thread = threading.Thread(target = Send_msg)
+send_thread.start()
+
+recv_thread = threading.Thread(target = Receive_msg)
+recv_thread.start()
---
+++ server.py
@@ -0,0 +1,87 @@
+import socket
+import threading
+
+SERVER = "127.0.0.1"
+PORT = 14000
+HEADER = 100
+FORMAT = "utf-8"
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((SERVER, PORT))
+server_socket.listen()
+
+#client_sockets_list = []
+clients = {}
+print("! Server Online !")
+
+
+def Receive_msg(conn):
+    username_header = conn.recv(HEADER)
+    username_length = int(username_header.decode(FORMAT))
+    username = connection.recv(username_length).decode(FORMAT)
+
+    #client_sockets_list.append(conn)
+    while True:
+        try:
+            msg_header = conn.recv(HEADER)
+            if msg_header:
+                msg_length = int(msg_header.decode(FORMAT))
+                message = conn.recv(msg_length).decode(FORMAT)
+
+                #client_sockets_list.append(conn)
+                print(f"{username} > {message}")
+                clients[conn] = {username: message}
+                
+                send_thread = threading.Thread(target = Send_msg, args = (conn, username, message))
+                send_thread.start()
+
+        except ConnectionResetError:
+            try:
+                del(clients[conn])
+
+                Send_msg(conn, username, "!DISCONNECTED!")
+
+            except RuntimeError:
+                print("A 'RuntimeError' occurred!")
+                continue
+
+            print(f"{username} DISCONNECTED!")
+            break
+
+
+def Send_msg(socket, user_name, message):
+    if message == "!DISCONNECTED!":
+        for client in clients:
+            username2 = user_name.encode(FORMAT)
+            user2_length = str(len(user_name)).encode(FORMAT)
+            user2_length += b" " * (HEADER - len(user2_length))
+
+            msg2 = message.encode(FORMAT)
+            message2_header = str(len(message)).encode(FORMAT)
+            message2_header += b" " * (HEADER - len(message2_header))
+
+            client.send(user2_length + username2 + message2_header + msg2)
+
+    else:
+        for client in clients:
+            try:
+                if client != socket:
+                    username = user_name.encode(FORMAT)
+                    user_length = str(len(user_name)).encode(FORMAT)
+                    user_length += b" " * (HEADER - len(user_length))
+
+                    msg = message.encode(FORMAT)
+                    message_header = str(len(message)).encode(FORMAT)
+                    message_header += b" " * (HEADER - len(message_header))
+
+                    client.send(user_length + username + message_header + msg)
+
+            except ConnectionResetError:
+                print("!CLIENT DISCONNECTED!")
+
+
+while True:
+    connection, address = server_socket.accept()
+    recv_thread = threading.Thread(target = Receive_msg, args = (connection, ))
+    recv_thread.start()
+    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
